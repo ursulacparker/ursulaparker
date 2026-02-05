a74496a045ed442c25f034f13edcba37490ec312
Ursula Parker <ucp4496@rit.edu>
2026-02-05 14:54:50 -0500

    added analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import Navbar from "@/components/Navbar";
 import { Raleway } from 'next/font/google';
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
 
 const raleway = Raleway({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] });
 
@@ -10,6 +11,7 @@ export default function App({ Component, pageProps }: AppProps) {
     <div className={raleway.className}>
       <Navbar />
       <Component {...pageProps} />
+      <Analytics />
     </div>
   );
 }
